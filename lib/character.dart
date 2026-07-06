8c89ea394b2701f8d76ed78f6937d10013be5f2c
se05503 <seyeongb18@gmail.com>
2026-07-06 22:48:48 +0900

    design: 위젯 레이아웃 4차 - 최상위 부모에 패딩 추가

--- lib/character.dart
+++ lib/character.dart
@@ -12,105 +12,108 @@ class Character extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 120, 77, 4),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Column(
-                children: [
-                  Image.asset("images/di1.png", height: 152),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 10),
-                    child: Container(
-                      width: 115,
-                      height: 2,
-                      color: Colors.deepPurple,
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Image.asset("images/di1.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
                     ),
-                  ),
-                  Text("Tyrannosaurus rex", style: TextStyle(fontSize: 18)),
-                ],
-              ),
-              Column(
-                children: [
-                  Image.asset("images/di2.png", height: 152),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 10),
-                    child: Container(
-                      width: 115,
-                      height: 2,
-                      color: Colors.deepPurple,
+                    Text("Tyrannosaurus rex", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset("images/di2.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
                     ),
-                  ),
-                  Text("Brachiosaurus", style: TextStyle(fontSize: 18)),
-                ],
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Column(
-                children: [
-                  Image.asset("images/di3.png", height: 152),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 10),
-                    child: Container(
-                      width: 115,
-                      height: 2,
-                      color: Colors.deepPurple,
+                    Text("Brachiosaurus", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Image.asset("images/di3.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
                     ),
-                  ),
-                  Text("Triceratops", style: TextStyle(fontSize: 18)),
-                ],
-              ),
-              Column(
-                children: [
-                  Image.asset("images/di4.png", height: 152),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 10),
-                    child: Container(
-                      width: 115,
-                      height: 2,
-                      color: Colors.deepPurple,
+                    Text("Triceratops", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset("images/di4.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
                     ),
-                  ),
-                  Text("Ankylosaurus", style: TextStyle(fontSize: 18)),
-                ],
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Column(
-                children: [
-                  Image.asset("images/di5.png", height: 152),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 10),
-                    child: Container(
-                      width: 115,
-                      height: 2,
-                      color: Colors.deepPurple,
+                    Text("Ankylosaurus", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Image.asset("images/di5.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
                     ),
-                  ),
-                  Text("Velociraptor", style: TextStyle(fontSize: 18)),
-                ],
-              ),
-              Column(
-                children: [
-                  Image.asset("images/di6.png", height: 152),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 10),
-                    child: Container(
-                      width: 115,
-                      height: 2,
-                      color: Colors.deepPurple,
+                    Text("Velociraptor", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Image.asset("images/di6.png", height: 152),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10),
+                      child: Container(
+                        width: 115,
+                        height: 2,
+                        color: Colors.deepPurple,
+                      ),
                     ),
-                  ),
-                  Text("Elasmosaurus", style: TextStyle(fontSize: 18)),
-                ],
-              ),
-            ],
-          ),
-        ],
+                    Text("Elasmosaurus", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
