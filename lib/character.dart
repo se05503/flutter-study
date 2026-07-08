564a31f6f20ddf7012907e85b69ddeae9b313768
se05503 <seyeongb18@gmail.com>
2026-07-08 15:19:39 +0900

    design: 레이아웃 UI 디자인 - 세 번째 row 하위에 CircleAvatar 추가

--- lib/character.dart
+++ lib/character.dart
@@ -6,113 +6,124 @@ class Character extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
+      backgroundColor: Colors.amber[900],
       appBar: AppBar(
         title: Text("DINOSAUR CARD", style: TextStyle(color: Colors.white)),
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 120, 77, 4),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Column(
-                  children: [
-                    Image.asset("images/di1.png", height: 152),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, bottom: 10),
-                      child: Container(
-                        width: 115,
-                        height: 2,
-                        color: Colors.deepPurple,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Image.asset("images/di1.png", height: 152),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, bottom: 10),
+                        child: Container(
+                          width: 115,
+                          height: 2,
+                          color: Colors.deepPurple,
+                        ),
                       ),
-                    ),
-                    Text("Tyrannosaurus rex", style: TextStyle(fontSize: 18)),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Image.asset("images/di2.png", height: 152),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, bottom: 10),
-                      child: Container(
-                        width: 115,
-                        height: 2,
-                        color: Colors.deepPurple,
+                      Text("Tyrannosaurus rex", style: TextStyle(fontSize: 18)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Image.asset("images/di2.png", height: 152),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, bottom: 10),
+                        child: Container(
+                          width: 115,
+                          height: 2,
+                          color: Colors.deepPurple,
+                        ),
                       ),
-                    ),
-                    Text("Brachiosaurus", style: TextStyle(fontSize: 18)),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Column(
-                  children: [
-                    Image.asset("images/di3.png", height: 152),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, bottom: 10),
-                      child: Container(
-                        width: 115,
-                        height: 2,
-                        color: Colors.deepPurple,
+                      Text("Brachiosaurus", style: TextStyle(fontSize: 18)),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Image.asset("images/di3.png", height: 152),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, bottom: 10),
+                        child: Container(
+                          width: 115,
+                          height: 2,
+                          color: Colors.deepPurple,
+                        ),
                       ),
-                    ),
-                    Text("Triceratops", style: TextStyle(fontSize: 18)),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Image.asset("images/di4.png", height: 152),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, bottom: 10),
-                      child: Container(
-                        width: 115,
-                        height: 2,
-                        color: Colors.deepPurple,
+                      Text("Triceratops", style: TextStyle(fontSize: 18)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Image.asset("images/di4.png", height: 152),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, bottom: 10),
+                        child: Container(
+                          width: 115,
+                          height: 2,
+                          color: Colors.deepPurple,
+                        ),
                       ),
-                    ),
-                    Text("Ankylosaurus", style: TextStyle(fontSize: 18)),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Column(
-                  children: [
-                    Image.asset("images/di5.png", height: 152),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, bottom: 10),
-                      child: Container(
-                        width: 115,
-                        height: 2,
-                        color: Colors.deepPurple,
+                      Text("Ankylosaurus", style: TextStyle(fontSize: 18)),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Image.asset("images/di5.png", height: 152),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, bottom: 10),
+                        child: Container(
+                          width: 115,
+                          height: 2,
+                          color: Colors.deepPurple,
+                        ),
                       ),
-                    ),
-                    Text("Velociraptor", style: TextStyle(fontSize: 18)),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Image.asset("images/di6.png", height: 152),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20, bottom: 10),
-                      child: Container(
-                        width: 115,
-                        height: 2,
-                        color: Colors.deepPurple,
+                      Text("Velociraptor", style: TextStyle(fontSize: 18)),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Image.asset("images/di6.png", height: 152),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20, bottom: 10),
+                        child: Container(
+                          width: 115,
+                          height: 2,
+                          color: Colors.deepPurple,
+                        ),
                       ),
-                    ),
-                    Text("Elasmosaurus", style: TextStyle(fontSize: 18)),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                      Text("Elasmosaurus", style: TextStyle(fontSize: 18)),
+                    ],
+                  ),
+                ],
+              ),
+              CircleAvatar(
+                backgroundImage: AssetImage("images/codingchef3.png"),
+                radius: 30,
+                backgroundColor: Colors.amber[200],
+              ),
+            ],
+          ),
         ),
       ),
     );
