2e2485951508e006c5b545969ab66e939d0cf186
se05503 <seyeongb18@gmail.com>
2026-07-20 11:40:22 +0900

    feat: 로또 번호 생성하기

--- lotto_generator/lib/lotto.dart
+++ lotto_generator/lib/lotto.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Lotto extends StatefulWidget {
   const Lotto({super.key});
@@ -10,6 +11,19 @@ class Lotto extends StatefulWidget {
 class _LottoState extends State<Lotto> {
   List<List<int>> lottoList = [];
 
+  void createNumber() {
+    List<int> lottoNumber = [];
+    while (true) {
+      int randomNumber = Random().nextInt(45) + 1;
+      if (!lottoNumber.contains(randomNumber)) lottoNumber.add(randomNumber);
+      if (lottoNumber.length == 6) break;
+    }
+    lottoNumber.sort();
+    setState(() {
+      lottoList.add(lottoNumber); // A,B,C,D,E 총 5번 추가하기
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +33,22 @@ class _LottoState extends State<Lotto> {
         title: Text("Lotto app"),
         centerTitle: true,
       ),
-      body: Center(child: Text("Hello my lotto app")),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.separated(
+            itemBuilder: (context, index) {
+              return Text(lottoList[index].toString());
+            },
+            separatorBuilder: (context, index) {
+              return Divider(color: Colors.black, thickness: 0.5);
+            },
+            itemCount: lottoList.length,
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: createNumber,
         backgroundColor: Colors.lightGreen,
         child: Icon(Icons.add, color: Colors.white),
       ),
