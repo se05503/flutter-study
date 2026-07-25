800dd5e207c0a47ca25816aad32d96bb2ed8ddc8
se05503 <seyeongb18@gmail.com>
2026-07-25 18:10:36 +0900

    feat: 사용자 회원가입 기능 - FirebaseAuthException 예외처리

--- firebase_login/lib/signup_screen.dart
+++ firebase_login/lib/signup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -10,6 +11,9 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,6 +52,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   child: TextField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.grey.shade400),
@@ -66,6 +72,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
                   child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
                     decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.grey.shade400),
@@ -81,16 +89,52 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   ).animate().fade(delay: 2000.ms),
                 ),
                 SizedBox(height: 24),
-                Container(
-                  width: 120,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white),
-                    color: Colors.orange[300],
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Icon(Icons.arrow_forward, color: Colors.white),
-                ).animate().fade(delay: 2000.ms),
+                GestureDetector(
+                  onTap: () async {
+                    try {
+                      UserCredential newUser = await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                            email: emailController.text,
+                            password: passwordController.text,
+                          );
+                      if (newUser.user != null) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("회원가입이 성공했습니다"),
+                            backgroundColor: Colors.blue,
+                          ),
+                        );
+                        emailController.clear();
+                        passwordController.clear();
+                      }
+                    } on FirebaseAuthException catch (e) {
+                      String msg = switch (e.code) {
+                        'email-already-in-use' => "이미 사용 중인 이메일입니다",
+                        'invalid-email' => "이메일 형식이 올바르지 않습니다",
+                        'weak-password' => "비밀번호가 너무 단순합니다",
+                        'network-request-failed' => "네트워크 연결을 확인해주세요",
+                        _ => "오류가 발생했습니다",
+                      };
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(msg),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                    }
+                  },
+                  child: Container(
+                    width: 120,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      color: Colors.orange[300],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+
+                    child: Icon(Icons.arrow_forward, color: Colors.white),
+                  ).animate().fade(delay: 2000.ms),
+                ),
                 SizedBox(height: 16),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
