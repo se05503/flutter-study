4bee8454b6da071029f9becb699d32829ab4b44a
se05503 <seyeongb18@gmail.com>
2026-07-25 19:27:50 +0900

    feat: 로그아웃 기능 - 서버 통신이 아닌 로컬 기기에서 인증 토큰을 삭제하는 방식 - 예외 처리X - 네트워크 없어도 로그아웃 됨

--- firebase_login/lib/dashboard_page.dart
+++ firebase_login/lib/dashboard_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
@@ -8,7 +9,20 @@ class DashboardPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("대시보드 화면"),
+        automaticallyImplyLeading: false,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text("로그아웃 되었습니다")));
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.exit_to_app, color: Colors.black),
+          ),
+        ],
       ),
       body: Center(child: Text("대시보드 화면입니다.", style: TextStyle(fontSize: 26))),
     );
