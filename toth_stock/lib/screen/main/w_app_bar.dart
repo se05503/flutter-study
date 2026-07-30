103e9004f192027df25b72137e1a30ae212718f8
se05503 <seyeongb18@gmail.com>
2026-07-30 11:23:16 +0900

    design: 앱바 UI 디자인 - Stack 위젯 - Positioned.fill 위젯

---
+++ toth_stock/lib/screen/main/w_app_bar.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class TossAppBar extends StatefulWidget {
+  const TossAppBar({super.key});
+
+  @override
+  State<TossAppBar> createState() => _TossAppBarState();
+}
+
+class _TossAppBarState extends State<TossAppBar> {
+  bool _showRedDot = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        height: 60,
+        color: Colors.black45,
+        child: Row(
+          children: [
+            SizedBox(width: 16),
+            Image.asset(
+              "assets/image/ic_appbar_toss.png",
+              height: 36,
+              color: Colors.grey,
+              colorBlendMode: BlendMode.srcIn,
+            ),
+            Expanded(child: Container()),
+            SvgPicture.asset(
+              'assets/image/ic_map.svg',
+              width: 24,
+              height: 24,
+              colorFilter: const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+            ),
+            SizedBox(width: 8),
+            Stack(
+              children: [
+                SvgPicture.asset(
+                  'assets/image/ic_notification.svg',
+                  width: 24,
+                  height: 24,
+                  colorFilter: const ColorFilter.mode(
+                    Colors.grey,
+                    BlendMode.srcIn,
+                  ),
+                ),
+                if (_showRedDot)
+                  Positioned.fill(
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        width: 6,
+                        height: 6,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            SizedBox(width: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
