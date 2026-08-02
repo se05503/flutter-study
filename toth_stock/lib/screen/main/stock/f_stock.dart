224ebe3a748c7ce76117a6f779b6e8f24e96c3ea
se05503 <seyeongb18@gmail.com>
2026-08-02 16:25:08 +0900

    design: 주식 탭 디자인 뼈대 - SliverAppBar의 actions 속성 사용 - SliverToBoxAdapter을 통해 위젯 변환 - getter로 위젯 뼈대 생성

--- toth_stock/lib/screen/main/stock/f_stock.dart
+++ toth_stock/lib/screen/main/stock/f_stock.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../common/color/colors.dart';
 
 class StockFragment extends StatefulWidget {
   const StockFragment({super.key});
@@ -10,6 +12,36 @@ class StockFragment extends StatefulWidget {
 class _StockFragmentState extends State<StockFragment> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          backgroundColor: AbstractThemeColors.appBarBackground,
+          actions: [
+            Image.asset("assets/image/ic_stock_search.png", width: 30, height: 30),
+            SizedBox(width: 10,),
+            Image.asset("assets/image/ic_stock_calendar.png", width: 30, height: 30),
+            SizedBox(width: 10,),
+            Image.asset("assets/image/ic_stock_settings.png", width: 30, height: 30),
+            SizedBox(width: 10,),
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              title,
+              tabBar,
+              myAccount,
+              SizedBox(height: 20,),
+              interestedStocks
+            ],
+          ),
+        )
+      ],
+    );
   }
+  Widget get title => Placeholder();
+  Widget get tabBar => Placeholder();
+  Widget get myAccount => Placeholder();
+  Widget get interestedStocks => Placeholder();
 }
