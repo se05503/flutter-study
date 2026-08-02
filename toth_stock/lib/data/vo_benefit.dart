d46f65a325266f54e12a301c6995a391dbc00c57
se05503 <seyeongb18@gmail.com>
2026-08-02 14:54:27 +0900

    feat: 혜택 모델링, 더미 데이터 추가

---
+++ toth_stock/lib/data/vo_benefit.dart
@@ -0,0 +1,16 @@
+class Benefit {
+  final String imagePath;
+  final String title;
+  final String subTitle;
+
+  Benefit({required this.imagePath, required this.title, required this.subTitle});
+}
+
+final benefitDummies = <Benefit>[
+  Benefit(imagePath: "assets/image/ic_benefit_hand", title: "포인트 받기", subTitle: "친구와 함께 토스 켜고"),
+  Benefit(imagePath: "assets/image/ic_benefit_mark", title: "얼마 받을지 보기", subTitle: "이번 주 미션하면"),
+  Benefit(imagePath: "assets/image/ic_benefit_luck", title: "포인트 받기", subTitle: "오늘의 행운복권"),
+  Benefit(imagePath: "assets/image/ic_benefit_live", title: "포인트 받기", subTitle: "라이브 쇼핑 보고"),
+  Benefit(imagePath: "assets/image/ic_benefit_quiz", title: "추가 혜택 보기", subTitle: "행운퀴즈 종료"),
+  Benefit(imagePath: "assets/image/ic_benefit_walk", title: "140원 받기", subTitle: "만보기"),
+];
