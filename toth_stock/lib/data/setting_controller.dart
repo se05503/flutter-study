d439570d28b4d9eaf16b78b287fa1eb3f6a38b8a
se05503 <seyeongb18@gmail.com>
2026-08-10 23:53:03 +0900

    feat: 앱 화면 밝기 조절하기 - screen_brightness 패키지 추가 - setApplicationScreenBrightness 메소드를 활용해 앱 내 밝기 조절 (시스템 화면 밝기 조절은 유저 경험상 좋지 않음) - try~catch 문으로 에러 핸들링

--- toth_stock/lib/data/setting_controller.dart
+++ toth_stock/lib/data/setting_controller.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:screen_brightness/screen_brightness.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingController extends GetxController {
   static const String _prefPushNotificationKey = "isPushNotificationSwitchOn";
-  static const String _prefScreenBrightnessKey = "soundVolume";
+  static const String _prefScreenBrightnessKey = "screenBrightness";
+
   final RxBool isPushNotificationOn = false.obs;
   final RxDouble screenBrightness = 0.5.obs;
 
@@ -15,8 +18,17 @@ class SettingController extends GetxController {
 
   Future<void> _loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
-    isPushNotificationOn.value = prefs.getBool(_prefPushNotificationKey) ?? false;
+    isPushNotificationOn.value =
+        prefs.getBool(_prefPushNotificationKey) ?? false;
     screenBrightness.value = prefs.getDouble(_prefScreenBrightnessKey) ?? 0.5;
+    try {
+      await ScreenBrightness().setApplicationScreenBrightness(
+        screenBrightness.value,
+      );
+      debugPrint("밝기 변경 성공: ${screenBrightness.value}");
+    } catch (e) {
+      debugPrint("밝기 변경 실패: $e");
+    }
   }
 
   Future<void> togglePushNotification(bool value) async {
@@ -27,6 +39,11 @@ class SettingController extends GetxController {
 
   Future<void> slideScreenBrightness(double value) async {
     screenBrightness.value = value;
+    try {
+      await ScreenBrightness().setApplicationScreenBrightness(value);
+    } catch (e) {
+      debugPrint("밝기 변경 실패: $e");
+    }
     final prefs = await SharedPreferences.getInstance();
     await prefs.setDouble(_prefScreenBrightnessKey, value);
   }
--- toth_stock/windows/flutter/generated_plugin_registrant.cc
+++ toth_stock/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <screen_brightness_windows/screen_brightness_windows_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ScreenBrightnessWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenBrightnessWindowsPluginCApi"));
 }
