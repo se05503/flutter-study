a25f121d1e4b841ffb33914d58f2f5789446682a
se05503 <seyeongb18@gmail.com>
2026-08-10 22:26:14 +0900

    feat: 푸시 설정 스위치 값 내부 저장소에 저장하기 - shared_preferences 패키지 활용 - 앱을 나갔다 들어와도 이전 상태가 저장됨

--- toth_stock/lib/screen/main/stock/s_setting.dart
+++ toth_stock/lib/screen/main/stock/s_setting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:toth_stock/common/color/colors.dart';
 import 'package:toth_stock/screen/widget/w_setting_switch.dart';
 
@@ -11,6 +12,25 @@ class SettingScreen extends StatefulWidget {
 
 class _SettingScreenState extends State<SettingScreen> {
   bool isToggleOn = false;
+  final String _prefKey = "isPushNotificationSwitchOn";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSettings();
+  }
+
+  Future<void> _loadSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isToggleOn = prefs.getBool(_prefKey) ?? false;
+    });
+  }
+
+  Future<void> _saveSettings(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_prefKey, value);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +50,7 @@ class _SettingScreenState extends State<SettingScreen> {
               setState(() {
                 isToggleOn = value;
               });
+              _saveSettings(value);
             },
           ),
         ],
