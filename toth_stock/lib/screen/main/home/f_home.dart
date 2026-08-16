adbb5fde47286f2b095058f0603b623a8a26bec0
se05503 <seyeongb18@gmail.com>
2026-08-16 18:42:08 +0900

    feat: 애니메이션 적용 - 홈 화면, 앱바의 알림 아이콘 - flutter_animate 패키지 추가 - Stack을 SafeArea로 감싸 시스템 바 침범 방지 - slideY 및 shake 적용, repeat로 반복 재생

--- toth_stock/lib/screen/main/home/f_home.dart
+++ toth_stock/lib/screen/main/home/f_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:toth_stock/data/bank.dart';
 import 'package:toth_stock/screen/widget/w_app_bar.dart';
 import 'package:toth_stock/screen/widget/w_big_button.dart';
@@ -17,30 +18,50 @@ class HomeFragment extends StatefulWidget {
 class _HomeFragmentState extends State<HomeFragment> {
   @override
   Widget build(BuildContext context) {
-    final statusBarHeight = MediaQuery.of(context).padding.top;
     return Container(
       color: Colors.black,
-      child: Stack(children: [
-        RefreshIndicator(
-          onRefresh: () async {
-            await Future.delayed(Duration(seconds: 2));
-          },
-          edgeOffset: TossAppBar.appBarHeight,
-          child: SingleChildScrollView(padding: EdgeInsets.only(top: TossAppBar.appBarHeight + statusBarHeight + 20, bottom: MainScreen.bottomNavigationHeight), child: Column(children: [
-            BigButton("토스뱅크", onTap: () {}),
-            SizedBox(height: 12),
-            RoundedContainer(child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("자산", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18)),
-                SizedBox(height: 6),
-                ...bankAccounts.map((e) => BankAccountWidget(e))
-              ],
-            ))
-          ])),
+      child: SafeArea(
+        child: Stack(
+          children: [
+            RefreshIndicator(
+              onRefresh: () async {
+                await Future.delayed(Duration(seconds: 2));
+              },
+              edgeOffset: TossAppBar.appBarHeight,
+              child: SingleChildScrollView(
+                padding: EdgeInsets.only(
+                  top: TossAppBar.appBarHeight + 12,
+                  bottom: MainScreen.bottomNavigationHeight,
+                ),
+                child: Column(
+                  children: [
+                    BigButton("토스뱅크", onTap: () {}),
+                    SizedBox(height: 12),
+                    RoundedContainer(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "자산",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          SizedBox(height: 6),
+                          ...bankAccounts.map((e) => BankAccountWidget(e)),
+                        ],
+                      ),
+                    ),
+                  ],
+                ).animate().slideY(duration: 1500.ms).fadeIn(),
+              )
+            ),
+            TossAppBar(),
+          ],
         ),
-        TossAppBar()
-      ],),
+      ),
     );
   }
 }
--- toth_stock/lib/screen/widget/w_app_bar.dart
+++ toth_stock/lib/screen/widget/w_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:toth_stock/common/color/colors.dart';
 import 'package:toth_stock/screen/notification/s_notification.dart';
@@ -20,57 +21,58 @@ class _TossAppBarState extends State<TossAppBar> {
       child: Container(
         height: TossAppBar.appBarHeight,
         color: AbstractThemeColors.appBarBackground,
-        child: Row(
-          children: [
-            SizedBox(width: 16),
-            Image.asset(
-              "assets/image/ic_appbar_toss.png",
-              height: 36,
-              color: Colors.grey,
-              colorBlendMode: BlendMode.srcIn,
-            ),
-            Expanded(child: Container()),
-            SvgPicture.asset(
-              'assets/image/ic_map.svg',
-              width: 24,
-              height: 24,
-              colorFilter: const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
-            ),
-            SizedBox(width: 8),
-            Stack(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => NotificationScreen()));
-                  },
-                  child: SvgPicture.asset(
-                    'assets/image/ic_notification.svg',
-                    width: 24,
-                    height: 24,
-                    colorFilter: const ColorFilter.mode(
-                      Colors.grey,
-                      BlendMode.srcIn,
-                    ),
-                  ),
-                ),
-                if (_showRedDot)
-                  Positioned.fill(
-                    child: Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        width: 6,
-                        height: 6,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.red,
-                        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            children: [
+              Image.asset(
+                "assets/image/ic_appbar_toss.png",
+                height: 36,
+                color: Colors.grey,
+                colorBlendMode: BlendMode.srcIn,
+              ),
+              Expanded(child: Container()),
+              SvgPicture.asset(
+                'assets/image/ic_map.svg',
+                width: 24,
+                height: 24,
+                colorFilter: const ColorFilter.mode(Colors.grey, BlendMode.srcIn),
+              ),
+              SizedBox(width: 8),
+              Stack(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => NotificationScreen()));
+                    },
+                    child: SvgPicture.asset(
+                      'assets/image/ic_notification.svg',
+                      width: 24,
+                      height: 24,
+                      colorFilter: const ColorFilter.mode(
+                        Colors.grey,
+                        BlendMode.srcIn,
                       ),
                     ),
                   ),
-              ],
-            ),
-            SizedBox(width: 16),
-          ],
+                  if (_showRedDot)
+                    Positioned.fill(
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          width: 6,
+                          height: 6,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ).animate(onPlay: (controller) => controller.repeat()).shake(duration: 1000.ms, hz: 5).then().shake(duration: 2000.ms, hz: 0)
+            ],
+          ),
         ),
       ),
     );
