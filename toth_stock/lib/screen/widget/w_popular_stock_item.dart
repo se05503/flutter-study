e76faba638873a165670df5bc78986750c041746
se05503 <seyeongb18@gmail.com>
2026-08-17 15:13:58 +0900

    feat: 애니메이션 적용 - 인기 검색 목록 - animations 패키지 추가 - OpenContainer 위젯 활용 - openBuilder, closedBuilder

--- toth_stock/lib/screen/widget/w_popular_stock_item.dart
+++ toth_stock/lib/screen/widget/w_popular_stock_item.dart
@@ -1,22 +1,41 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:toth_stock/common/color/colors.dart';
 import 'package:toth_stock/data/vo_stock.dart';
+import 'package:toth_stock/screen/main/stock/s_stock_detail.dart';
 
 class PopularStockItem extends StatelessWidget {
   final int number;
   final PopularStock stock;
+
   const PopularStockItem(this.number, this.stock, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20.0),
-      child: Row(children: [
-        SizedBox(width: 40, child: Text(number.toString(), style: TextStyle(color: AbstractThemeColors.lessImportantColor))),
-        Text(stock.name, style: TextStyle(color: AbstractThemeColors.lessImportantColor, fontWeight: FontWeight.bold, fontSize: 16)),
-        Expanded(child: Spacer()),
-        Text(stock.todayPercentageString, style: TextStyle(color: stock.color, fontWeight: FontWeight.bold)),
-      ],),
+        padding: const EdgeInsets.symmetric(vertical: 20.0),
+        child:
+        OpenContainer<bool>(
+          openColor: AbstractThemeColors.roundedLayoutBackground,
+          closedColor: AbstractThemeColors.roundedLayoutBackground,
+          openBuilder: (BuildContext context, VoidCallback _) {
+            return StockDetailScreen(stock.name);
+          },
+          closedBuilder: (BuildContext context, VoidCallback action) {
+            return Row(children: [
+              SizedBox(width: 40,
+                  child: Text(number.toString(), style: TextStyle(
+                      color: AbstractThemeColors.lessImportantColor))),
+              Text(stock.name, style: TextStyle(
+                  color: AbstractThemeColors.lessImportantColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16)),
+              Expanded(child: Spacer()),
+              Text(stock.todayPercentageString, style: TextStyle(
+                  color: stock.color, fontWeight: FontWeight.bold)),
+            ],);
+          },
+        )
     );
   }
 }
